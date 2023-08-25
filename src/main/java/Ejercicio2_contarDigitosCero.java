bd1e1df53cc3a5303a3239e39ff3cae9d097fa10
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-08-24 23:05:17 -0500

    Se realiza el ejercicio numero 2 propuesto en clase

--- src/main/java/Ejercicio2_contarDigitosCero.java
+++ src/main/java/Ejercicio2_contarDigitosCero.java
@@ -1,13 +1,23 @@
 public class Ejercicio2_contarDigitosCero {
 
     public static void main(String[] args) {
-        int[] arreglo = {2,5,0,6,0};
+        int[] arreglo = {2,0,0,6,0};
+        int indice = 0;
         int resultado = 0;
-        resultado = contarDigitosCero(arreglo);
+        resultado = contarDigitosCero(arreglo, indice);
+        System.out.println("Hay "+ resultado + " ceros en el arreglo.");
     }
 
-    private static int contarDigitosCero(int[] arreglo) {
-
+    private static int contarDigitosCero(int[] arreglo, int n) {
+        if(n > arreglo.length-1){
+            return 0;
+        }else {
+            if( arreglo[n] == 0){
+                return contarDigitosCero(arreglo, n+1) + 1;
+            } else{
+                return contarDigitosCero(arreglo, n+1);
+            }
+        }
         
     }
 }
