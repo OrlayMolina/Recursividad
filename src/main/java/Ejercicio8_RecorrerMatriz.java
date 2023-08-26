352e87937312f50b677770cf26a54ce09428e8ce
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-08-26 18:55:19 -0500

    Se realiza el ejercicio 8

--- src/main/java/Ejercicio8_RecorrerMatriz.java
+++ src/main/java/Ejercicio8_RecorrerMatriz.java
@@ -2,20 +2,31 @@ public class Ejercicio8_RecorrerMatriz {
 
     public static void main(String[] args) {
 
-        int[][] matriz = {{1,2,3},{8,4,3},{5,6,8}};
+        int[][] matriz = {
+                {1,2,3},
+                {8,4,3},
+                {5,6,8}
+        };
         imprimirMatrizRecursiva(matriz, 0, 0);
     }
 
     private static void imprimirMatrizRecursiva(int[][] matriz, int fila, int columna) {
         //fila - columna
         if(fila > matriz.length-1 || columna > matriz[columna].length-1){
-            System.out.println("");
+            System.out.print("");
         }else{
-            if(){
-
+            System.out.print(matriz[fila][columna] + " ");
+            if(columna == matriz[columna].length-1){
+                //siguiente fila
+                ++fila;
+                //Reinicio columna
+                columna = 0;
+                //salto de linea
+                System.out.println("");
             }else{
-
+                ++columna;
             }
+            imprimirMatrizRecursiva(matriz, fila, columna);
         }
     }
 }
