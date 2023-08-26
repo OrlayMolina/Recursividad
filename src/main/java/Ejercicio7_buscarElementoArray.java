b2131ace094276635995092b6ff879ef48266b0c
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-08-26 18:22:32 -0500

    Se resuelve el ejercicio 7

--- src/main/java/Ejercicio7_buscarElementoArray.java
+++ src/main/java/Ejercicio7_buscarElementoArray.java
@@ -1,15 +1,57 @@
+import java.util.ArrayList;
+
 public class Ejercicio7_buscarElementoArray {
 
     public static void main(String[] args) {
         
-        int[] array = {5,6,7,12,3};
+        int[] array = {5,7,2,5,7};
         int inicial = 0;
         int numeroBuscado = 7;
-        int resultado = 0;
-        resultado = buscarElemento(array, inicial, array.length-1, numeroBuscado);
-        System.out.println("El numero "+numeroBuscado + " esta en la posición  " + resultado + " del array.");
+        ArrayList<Integer> posiciones = new ArrayList<>();
+        ArrayList<Integer> resultado = buscarElemento(array, inicial, array.length-1, numeroBuscado, posiciones);
+        System.out.print("La(s) posición(es) del array en donde se encuentra el numero "+ numeroBuscado + " son: " );
+        imprimirRecursivo(resultado, inicial, resultado.size()-1);
+
     }
 
-    private static int buscarElemento(int[] array, int inicial, int i, int numeroBuscado) {
+    /**
+     * 7. Recorrer un array de forma recursiva
+     * @param array
+     * @param inicial
+     * @param fin
+     * @param numeroBuscado
+     * @param posiciones
+     * @return
+     */
+    private static ArrayList<Integer> buscarElemento(int[] array, int inicial, int fin, int numeroBuscado, ArrayList<Integer> posiciones) {
+
+        if(inicial > fin){
+            return posiciones;
+        }else {
+            if(array[inicial] == numeroBuscado){
+                posiciones.add(inicial);
+                return buscarElemento(array, inicial+1, fin, numeroBuscado,posiciones);
+            }else {
+                return buscarElemento(array, inicial+1, fin, numeroBuscado, posiciones);
+            }
+        }
+    }
+
+    /**
+     * Método auxiliar para solucionar el ejercicio
+     * @param posiciones
+     * @param inicial
+     * @param fin
+     */
+    public static void imprimirRecursivo(ArrayList<Integer> posiciones, int inicial, int fin){
+
+        if(inicial == fin){
+            System.out.print(posiciones.get(inicial));
+        }else{
+            System.out.print(posiciones.get(inicial) + " , ");
+            imprimirRecursivo(posiciones, inicial+1, fin);
+
+        }
+
     }
 }
