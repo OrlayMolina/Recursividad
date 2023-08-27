7ea83f4a7733eed38151ccf04779f38ce810fbed
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-08-27 09:27:28 -0500

    Se realiza la solucion del ejercicio 1 (29)

--- src/main/java/Ejercicio29_determinarMayorArrayRetrocediendo.java
+++ src/main/java/Ejercicio29_determinarMayorArrayRetrocediendo.java
@@ -1,2 +1,33 @@
 public class Ejercicio29_determinarMayorArrayRetrocediendo {
+
+    public static void main(String[] args) {
+
+        int[] vector = {8, 5, 5, 16, 2, 7, 12, 7, 6, 9};
+        int indice = 0;
+        int resultado = 0;
+        resultado = numeroMayorArreglo(vector, 0,vector.length-1);
+        System.out.println("El número mayor del arreglo es: " + resultado);
+    }
+
+    /**
+     * 29. Algoritmo recursivo que retorne el mayor de un arreglo de 10 elementos.
+     *     Recorrido de derecha a izquierda. Parámetros: (arreglo, mayor, indice)
+     * @param vector
+     * @param mayor indice inicial
+     * @param indice indice final
+     * @return
+     */
+    private static int numeroMayorArreglo(int[] vector, int mayor, int indice) {
+
+        if( indice < 0) {
+            return mayor;
+        }else {
+            if (vector[indice] > mayor) {
+                return numeroMayorArreglo(vector, vector[indice], --indice);
+            } else {
+                return numeroMayorArreglo(vector, mayor, --indice);
+            }
+        }
+
+    }
 }
