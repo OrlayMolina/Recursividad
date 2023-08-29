56922703b426015362c6d8907bfb2453acd6f25b
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-08-28 22:38:07 -0500

    Se realiza el ejercicio 28

--- src/main/java/Ejercicio28_determinarNormaVector.java
+++ src/main/java/Ejercicio28_determinarNormaVector.java
@@ -1,2 +1,26 @@
 public class Ejercicio28_determinarNormaVector {
+
+    public static void main(String[] args) {
+        double[] vector = {3.0, 4.0, 5.0};
+        double norma = calcularNorma(vector, vector.length - 1);
+        System.out.println("La norma del vector es: " + norma);
+    }
+
+    /**
+     * 28.  Escriba un método recursivo que retorne la norma de un vector (a) de tamaño n.
+     *      La norma de un vector se calcula sacando la raíz cuadrada a la sumatoria de cada
+     *      uno de sus elementos al cuadrado.
+     * @param vector
+     * @param indice
+     * @return
+     */
+    private static double calcularNorma(double[] vector, int indice) {
+        if (indice == 0) {
+            return Math.sqrt(vector[0] * vector[0]);
+        } else {
+            double elementoActual = vector[indice];
+            double normaRestante = calcularNorma(vector, indice - 1);
+            return Math.sqrt(elementoActual * elementoActual + normaRestante * normaRestante);
+        }
+    }
 }
