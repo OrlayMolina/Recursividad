2b9af5b6a323a71ac888120a66eab8c8b28659f1
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-09-14 18:04:24 -0500

    Se agrega la solución de matriz transpuesta

--- src/main/java/Ejercicio0_pila_fibonacci.java
+++ src/main/java/Ejercicio0_pila_fibonacci.java
@@ -3,13 +3,13 @@ public class Ejercicio0_pila_fibonacci {
     public static void main(String[] args) {
         int n = 5;
         int resultado = fib(n);
-        System.out.println(resultado);
     }
 
     public static int fib(int n) {
        if(n<=1)
            return n;
        else
+
            return fib(n-1) + fib(n -2);
 
     }
