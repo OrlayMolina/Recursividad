6eaaa5f5961ef4aa45dd3fce72e0788c6d731e77
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-09-14 19:09:31 -0500

    Se agrega condigo para tratar de solucionar la busqueda binaria

--- src/main/java/Ejercicio23_busquedaBinaria.java
+++ src/main/java/Ejercicio23_busquedaBinaria.java
@@ -1,2 +1,40 @@
+
+/*
+    La búsqueda binaria implica que un array esté ordenado, y el algoritmo consiste
+    en dividir el array por su elemento medio en dos subarrays más pequeños, y comparar
+    el elemento con el del centro. Si coinciden, la búsqueda se termina. Si el elemento
+    es menor, debe estar (si está) en el primer subarray, y si es mayor está en el segundo.
+    Escriba un método recursivo de búsqueda binaria.
+ */
 public class Ejercicio23_busquedaBinaria {
+
+    public static void main(String[] args) {
+
+        int[] array = {2, 4, 6, 8, 10, 12, 14};
+
+        System.out.println("Indices");
+        creacionArray(array);
+    }
+
+    /**
+     * Método auxiliar que permite identificar sí la cantidad de elementos que hay
+     * en el array es par o impar.
+     * @param array
+     * @return
+     */
+    public static boolean arrayPar(int[] array){
+        if(array.length % 2 == 0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public static void creacionArray(int[] array){
+        if(arrayPar(array)){
+            int indices = (array.length-1)/2;
+            int[] array1;
+            int[] array2;
+        }
+    }
 }
